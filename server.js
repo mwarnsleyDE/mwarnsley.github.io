ee7b3414a9cbc312b6dbdfa8e0f72a7b14c9f013
Marcus Warnsley <mwarnsley@Marcuss-MacBook-Pro.local>
2016-08-25 18:22:24 -0400

    Setting up Babel and webpack for React

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+//Include variables for express
 var express = require('express')
     port = 3000;
 
 //Create Application
 var app = express();
 
+//Tell Express which folder to serve
 app.use(express.static('public'));
 
+//Tell Express which port to listen for and start the server on
 app.listen(port, function(){
   console.log("Express Server is running on port: " + port);
 });
